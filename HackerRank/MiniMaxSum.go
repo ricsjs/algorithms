5a79029eb26fa222ca79cf12ae6bb93beff8ed04
Ricardo Alencar <ricfilho0007@gmail.com>
2024-01-09 21:08:32 -0300

    MiniMaxSum

---
+++ HackerRank/MiniMaxSum.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr []int32) {
+	// Write your code here
+	var intArr []int
+	for _, v := range arr {
+		intArr = append(intArr, int(v))
+	}
+
+	sort.Ints(intArr)
+
+	minSum := intArr[0] + intArr[1] + intArr[2] + intArr[3]
+
+	maxSum := intArr[1] + intArr[2] + intArr[3] + intArr[4]
+
+	fmt.Println(minSum, maxSum)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
