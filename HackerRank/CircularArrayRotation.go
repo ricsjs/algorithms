a609b543313abaa293a9946a284f91cd87820bc3
Ricardo Alencar <ricfilho0007@gmail.com>
2024-01-10 19:33:26 -0300

    remove main funcs

--- HackerRank/CircularArrayRotation.go
+++ HackerRank/CircularArrayRotation.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
 )
 
 /*
@@ -38,78 +33,4 @@ func circularArrayRotation(a []int32, k int32, queries []int32) []int32 {
 	}
 
 	return results
-}
-
-func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
-
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
-
-	defer stdout.Close()
-
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
-
-	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
-
-	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
-	checkError(err)
-	n := int32(nTemp)
-
-	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
-	checkError(err)
-	k := int32(kTemp)
-
-	qTemp, err := strconv.ParseInt(firstMultipleInput[2], 10, 64)
-	checkError(err)
-	q := int32(qTemp)
-
-	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
-
-	var a []int32
-
-	for i := 0; i < int(n); i++ {
-		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
-		checkError(err)
-		aItem := int32(aItemTemp)
-		a = append(a, aItem)
-	}
-
-	var queries []int32
-
-	for i := 0; i < int(q); i++ {
-		queriesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-		checkError(err)
-		queriesItem := int32(queriesItemTemp)
-		queries = append(queries, queriesItem)
-	}
-
-	result := circularArrayRotation(a, k, queries)
-
-	for i, resultItem := range result {
-		fmt.Fprintf(writer, "%d", resultItem)
-
-		if i != len(result)-1 {
-			fmt.Fprintf(writer, "\n")
-		}
-	}
-
-	fmt.Fprintf(writer, "\n")
-
-	writer.Flush()
-}
-
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
-
-	return strings.TrimRight(string(str), "\r\n")
-}
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
--- HackerRank/Staircase.go
+++ HackerRank/Staircase.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
 )
 
 /*
@@ -25,30 +20,5 @@ func staircase(n int32) {
 			fmt.Print("#")
 		}
 		fmt.Println()
-	}
-}
-
-func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
-
-	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	checkError(err)
-	n := int32(nTemp)
-
-	staircase(n)
-}
-
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
-
-	return strings.TrimRight(string(str), "\r\n")
-}
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
 	}
 }
